b98dfccf0dd1d2c6badc978897f1b5acab30b067
Eric Walker <ezmoswalker@gmail.com>
2026-02-18 21:40:27 -0500

    idk what arduino has done to my config

---
+++ lua/custom/plugins/arduino.lua
@@ -0,0 +1,11 @@
+return {
+  -- 'yuukiflow/Arduino-Nvim',
+  -- dependencies = {
+  --   'nvim-telescope/telescope.nvim',
+  --   'neovim/nvim-lspconfig',
+  -- },
+  -- ft = { 'arduino' },
+  -- init = function()
+  --   require 'Arduino-Nvim'
+  -- end,
+}
