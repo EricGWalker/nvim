4ec15bbb8b75cd92417b36d5e37d499968841c37
Eric Walker <ezmoswalker@gmail.com>
2026-02-25 17:46:59 -0500

    added documentation for rest and a single keybind

--- lua/custom/plugins/rest.lua
+++ lua/custom/plugins/rest.lua
@@ -9,6 +9,24 @@ return {
       require('telescope').load_extension 'rest'
       -- then use it, you can also use the `:Telescope rest select_env` command
       -- require('telescope').extensions.rest.select_env()
+      vim.keymap.set('n', '<leader>r', '<cmd>Rest run<CR>', { desc = 'REST: run request under cursor' })
     end,
   },
 }
+
+--[[
+---------------------------------------------------------------------------
+| User Command        | Behavior                                          |
+---------------------------------------------------------------------------
+|:Rest open           | Open result pane                                  |
+|:Rest run            | Run request under the cursor                      |
+|:Rest run {name}     | Run request with name {name}                      |
+|:Rest last           | Run last request                                  |
+|:Rest logs           | Edit logs file                                    |
+|:Rest cookies        | Edit cookies file                                 |
+|:Rest env show       | Show dotenv file registered to current .http file |
+|:Rest env select     | Select & register .env file with vim.ui.select()  |
+|:Rest env set {path} | Register .env file to current .http file          |
+---------------------------------------------------------------------------
+]]
+--
