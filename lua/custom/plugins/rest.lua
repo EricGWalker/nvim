4f63bff81e6400d63e8211eb508cb8855fc62016
Eric Walker <ezmoswalker@gmail.com>
2026-02-25 17:11:46 -0500

    updated lazy lock and added rest extension

---
+++ lua/custom/plugins/rest.lua
@@ -0,0 +1,14 @@
+return {
+  'rest-nvim/rest.nvim',
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      table.insert(opts.ensure_installed, 'http')
+      -- Enable Telescope rest extension
+      require('telescope').load_extension 'rest'
+      -- then use it, you can also use the `:Telescope rest select_env` command
+      -- require('telescope').extensions.rest.select_env()
+    end,
+  },
+}
